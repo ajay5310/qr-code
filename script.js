406d340f8c6b007c717a2dc5082062866c021080
ajay5310 <91721096+ajay5310@users.noreply.github.com>
2024-01-04 14:33:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+const wrapper = document.querySelector(".wrapper"),
+  qrInput = wrapper.querySelector(".form input"),
+  generateBtn = wrapper.querySelector(".form button"),
+  qrImg = wrapper.querySelector(".qr-code img");
+const  downloadBtn=document.querySelector(".d");
+let preValue;
+
+generateBtn.addEventListener("click", () => {
+  let qrValue = qrInput.value.trim();
+  if (!qrValue || preValue === qrValue) return;
+  preValue = qrValue;
+  generateBtn.innerText = "Generating QR Code...";
+  qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${qrValue}`;
+  qrImg.addEventListener("load", () => {
+    wrapper.classList.add("active");
+    generateBtn.innerText = "Generate QR Code";
+  });
+});
+
+qrInput.addEventListener("keyup", () => {
+  if (!qrInput.value.trim()) {
+    wrapper.classList.remove("active");
+    preValue = "";
+  }
+});
+
+downloadBtn.addEventListener("click", async () => {
+  const response = await fetch(qrImg.src);
+  const blob = await response.blob();
+  const downloadLink = document.createElement("a");
+  downloadLink.href = URL.createObjectURL(blob);
+  downloadLink.download = "qr-code.jpg";
+  downloadLink.click();
+});
